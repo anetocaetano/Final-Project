5a3c949161a1de2691667cf8ae8c66ce1dd6073e
anetocaetano <anetocaetano@gmail.com>
2017-05-31 20:13:48 -0600

    Initial Commit

---
+++ ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+   
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void BackBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void SettingsBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ ScreenResolutions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScreenResolutions : MonoBehaviour {
+    
+    public static void ScreenResolution1()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void ScreenResolution2()
+    {
+        Screen.SetResolution(1280, 720, true, 60);
+    }
+
+    public void ScreenResolution3()
+    {
+        Screen.SetResolution(1920, 1080, true, 60);
+    }
+}
